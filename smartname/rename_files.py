784347ea24b07b605a6242bc9dc7075171d5b080
Kimberly McNeil <146407938+mcneilkimberly@users.noreply.github.com>
2025-11-04 14:11:30 -0500

    Add files via upload
    Template of file renamer python script

---
+++ smartname/rename_files.py
@@ -0,0 +1,62 @@
+"""
+
+"""
+
+# CLI - Command-Line Interface
+def main():
+  pass
+
+
+
+# API - Application Programming Interface
+def call_ollama_vision():
+  pass
+
+
+# API 
+def call_ollama_text():
+  pass
+
+
+# Extractor
+def extract_pdf_image():
+  pass
+
+
+# Extractor
+def extract_docx_content():
+  pass
+
+
+# Extractor
+def extract_pptx_content():
+  pass
+
+
+# Extractor
+def extract_video_frame():
+  pass
+
+
+# Extractor
+def read_text_snippet():
+  pass
+
+
+# Routing
+def generate_filename_for_file():
+  pass
+
+
+# UX - User Experience
+def apply_casing():
+  pass
+
+# UX
+def sanitize_filename():
+  pass
+
+
+# Run main
+if __name__ == "__main__":
+    main()
